9a24485ff4666cae67205a185ae4779c2afd86be
razanalk <13238@holbertonstudents.com>
2026-02-24 20:07:10 +0000

    Fix betty documentation properly

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -7,31 +7,31 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-	int last_digit;
+int n;
+int last_digit;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	last_digit = n % 10;
+last_digit = n % 10;
 
-	printf("Last digit of %d is %d", n, last_digit);
+printf("Last digit of %d is %d", n, last_digit);
 
-	if (last_digit > 5)
-	{
-		printf(" and is greater than 5");
-	}
-	else if (last_digit == 0)
-	{
-		printf(" and is 0");
-	}
-	else
-	{
-		printf(" and is less than 6 and not 0");
-	}
+if (last_digit > 5)
+{
+printf(" and is greater than 5");
+}
+else if (last_digit == 0)
+{
+printf(" and is 0");
+}
+else
+{
+printf(" and is less than 6 and not 0");
+}
 
-	printf("\n");
+printf("\n");
 
-	return (0);
+return (0);
 }
 
