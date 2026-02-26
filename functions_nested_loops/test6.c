e24c6fe020b957f97e7f94aed02332bc54a63dea
razanalk <13238@holbertonstudents.com>
2026-02-26 21:00:21 +0000

    Task 6 abs

---
+++ functions_nested_loops/test6.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", _abs(-1));
+	printf("%d\n", _abs(0));
+	printf("%d\n", _abs(1));
+	printf("%d\n", _abs(-98));
+	return (0);
+}
