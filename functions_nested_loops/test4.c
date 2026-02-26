30593952397ba2ba5176b814acbd8d20949b46aa
razanalk <13238@holbertonstudents.com>
2026-02-26 20:51:35 +0000

    Task 4 isalpha

---
+++ functions_nested_loops/test4.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	_putchar(_isalpha('H') + '0');
+	_putchar(_isalpha('o') + '0');
+	_putchar(_isalpha(108) + '0');
+	_putchar(_isalpha(';') + '0');
+	_putchar('\n');
+	return (0);
+}
