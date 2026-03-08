7f312fef5b050f38e9611bf2601e890feac28cf5
razanalk <13238@holbertonstudents.com>
2026-03-08 21:15:13 +0000

    Task 3: _strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to check
+ * @accept: accepted characters
+ *
+ * Return: number of bytes in initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+	int found;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		found = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+
+		if (found == 0)
+			break;
+
+		count++;
+	}
+
+	return (count);
+}
