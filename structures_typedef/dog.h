3d58d23d43dc4f8d7cdd748833cd23ab480e622b
razanalk <1323@holbertonstudents.com>
2026-03-31 15:15:34 +0000

    Add init_dog prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,4 +14,13 @@ float age;
 char *owner;
 };
 
+/**
+ * init_dog - initialize struct dog
+ * @d: pointer to struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
