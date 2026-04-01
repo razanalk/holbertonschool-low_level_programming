5274dc8979c2a353386f91ca90ca43bc3e14db64
razanalk <13238@holbertonstudents.com>
2026-04-01 15:32:13 +0000

    Add new_dog prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,5 +17,7 @@ char *owner;
 
 typedef struct dog dog_t;
 void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
