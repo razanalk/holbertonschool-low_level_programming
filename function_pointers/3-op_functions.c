59c0ebfff1aa692370c7ed3c57f081ddc21c96d2
razanalk <13238@holbertonstuudents.com>
2026-04-06 15:46:15 +0000

    Fix betty documentation for op functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,25 +1,53 @@
-#include "3-calc.h"
-
+/**
+ * op_add - adds two integers
+ * @a: first number
+ * @b: second number
+ * Return: result of addition
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - subtracts two integers
+ * @a: first number
+ * @b: second number
+ * Return: result of subtraction
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - multiplies two integers
+ * @a: first number
+ * @b: second number
+ * Return: result of multiplication
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - divides two integers
+ * @a: first number
+ * @b: second number
+ * Return: result of division
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
+/**
+ * op_mod - remainder of division
+ * @a: first number
+ * @b: second number
+ * Return: remainder
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
