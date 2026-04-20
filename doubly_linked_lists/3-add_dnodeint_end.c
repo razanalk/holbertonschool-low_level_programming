68d2499e67a2ddb737368dd3d0da4fa3d885291c
razanalk <13238@holbertonstudents.com>
2026-04-20 14:32:19 +0000

    fix betty doc

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: pointer to the head of the list
+ * @n: integer value to add
+ *
+ * Return: address of the new element, or NULL if it fails
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
