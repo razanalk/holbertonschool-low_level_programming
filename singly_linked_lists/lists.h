f2c337d323293bac45a7de9c9b2a93118e644538
razanalk <13238@holbertonstudents.com>
2026-04-20 10:26:10 +0000

    fix prototype for list_len

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -7,15 +7,15 @@
  * struct list_s - singly linked list
  * @str: string
  * @len: length of the string
- * @next: pointer to next node
+ * @next: next node
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
