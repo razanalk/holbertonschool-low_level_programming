606898f33b2cf67cc5b8ac4b98bc6f8df9f5c3ea
Razanalk <13238@holbertonstudents.com>
2026-04-22 14:31:57 +0000

    Task 1: create file

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+HEAD
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+
+#include <stddef.h>
+#include <sys/types.h>
+
+/* Task 0 */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Task 1 */
+int create_file(const char *filename, char *text_content);
+
+/* Task 2 */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* Task 3 */
+int cp(char *file_from, char *file_to);
 
 #endif
