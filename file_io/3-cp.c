7199fa6a2b6d4153f7abfa17723e68a654d6875d
Razanalk <13238@holbertonstudents.com>
2026-04-22 14:54:29 +0000

    fix cp output order

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,21 +19,22 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
+		_exit(97);
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		_exit(98);
 	}
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		close(fd_from);
+		_exit(99);
 	}
 
 	while ((r = read(fd_from, buffer, 1024)) > 0)
@@ -44,7 +45,7 @@ int main(int argc, char *argv[])
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			exit(99);
+			_exit(99);
 		}
 	}
 
@@ -53,19 +54,19 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
-		exit(98);
+		_exit(98);
 	}
 
 	if (close(fd_from) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
+		_exit(100);
 	}
 
 	if (close(fd_to) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		_exit(100);
 	}
 
 	return (0);
