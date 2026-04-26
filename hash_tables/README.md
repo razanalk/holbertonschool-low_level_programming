3c206280070df6eccc2cca18ce3b71b81a301c6f
razanalk <13238holbeertonschool@gmail.com>
2026-04-26 14:21:07 +0000

    Task 0: hash_table_create

---
+++ hash_tables/README.md
@@ -0,0 +1,14 @@
+# Hash Tables
+
+## Task 0: hash_table_create
+
+Creates a hash table.
+
+### Prototype
+hash_table_t *hash_table_create(unsigned long int size);
+
+### Description
+- Allocates memory for hash table
+- Initializes array with NULL
+- Returns pointer to table
+- Returns NULL on failure
---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,32 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key
+ * @value: The value
+ * @next: Pointer to next node
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table
+ * @size: size of array
+ * @array: array of nodes
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
